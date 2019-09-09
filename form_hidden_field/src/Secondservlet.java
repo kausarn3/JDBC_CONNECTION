5954dfb813567816b0d51aad349800c8356d9321
Kausar Nazir <32454400+kausarn3@users.noreply.github.com>
2019-09-10 00:10:33 +0530

    Update Secondservlet.java

--- form_hidden_field/src/Secondservlet.java
+++ form_hidden_field/src/Secondservlet.java
@@ -18,13 +18,16 @@ public class Secondservlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter w=response.getWriter();
+		//fetch input data from Firstservlet.
 		String s1=request.getParameter("t1");
 		String s2=request.getParameter("t2");
 		String s3=request.getParameter("t3");
+		//response from browser to display form.
 		w.println("<form action='Thirdservlet' method='post'>");
 		w.println("Skills:<input type='text' name='t4'><br><br>");
 		w.println("Salary:<input type='text' name='t5'><br><br>");
 		w.println("<input type='submit'>");
+		//stores the fetch data from Firstservlet in hidden fields.
 		w.println("<input type='hidden' name='t1' value="+"'"+s1+"'>");
 		w.println("<input type='hidden' name='t2' value="+"'"+s2+"'>");
 		w.println("<input type='hidden' name='t3' value="+"'"+s3+"'>");
